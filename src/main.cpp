27d8b62b37caf51c85e1c34c1df9148a53a0475f
mrdel1r1ous <alexsmirnov7428@gmail.com>
2025-11-13 23:04:57 +0200

    Added isDigit()
    isDigit() is used in countChars() as a condition

--- src/main.cpp
+++ src/main.cpp
@@ -4,9 +4,14 @@
 #include "data.h"
 #include "operations.h"
 
+bool isDigit(char c) {
+	if (isdigit(c)) return true;
+	return false;
+}
+
 int main(int argc, char *argv[]) {
 	auto line = lab::createLine("7th_lab");
-	int x = lab::countChars(line, isdigit());
+	int x = lab::countChars(line, isDigit());
 	std::cout << line->content << std::endl;
 	std::cout << x << std::endl;
 	lab::deleteLine(line);
