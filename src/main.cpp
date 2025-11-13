b4262c4a04e6150c1e73be4d3cad939b95458f87
mrdel1r1ous <alexsmirnov7428@gmail.com>
2025-11-13 23:11:23 +0200

    Updated main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -11,7 +11,7 @@ bool isDigit(char c) {
 
 int main(int argc, char *argv[]) {
 	auto line = lab::createLine("7th_lab");
-	int x = lab::countChars(line, isDigit());
+	int x = lab::countChars(line, isDigit);
 	std::cout << line->content << std::endl;
 	std::cout << x << std::endl;
 	lab::deleteLine(line);
