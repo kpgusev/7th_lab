235700d7d14975e4f82c299b69d0de04e11ff415
kpgusev <kpgusev@users.noreply.github.com>
2025-11-13 23:20:06 +0200

    Add feature test

--- src/main.cpp
+++ src/main.cpp
@@ -12,7 +12,7 @@ bool isDigit(char c) {
 int main(int argc, char *argv[]) {
 	auto line = lab::createLine("7th_lab");
 	int x = lab::countChars(line, isDigit);
-	std::cout << line->content << std::endl;
+	std::cout << line->content << " (" << x << ")" << std::endl;
 	lab::deleteLine(line);
 	return EXIT_SUCCESS;
 }
