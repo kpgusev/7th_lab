d31c33581de5bcd1d674f98cfbae0577fd7616a0
mrdel1r1ous <alexsmirnov7428@gmail.com>
2025-11-13 22:54:05 +0200

    Added countChars()

--- include/operations.h
+++ include/operations.h
@@ -4,9 +4,6 @@
 
 namespace lab {
 
-void transformLine(TextLine* line, char (*transform)(char));
-
-char toUpper(char c);
-char rot13(char c);
+	int countChars(const TextLine* line, bool (*condition)(char));
 
 } // namespace lab
