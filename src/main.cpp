be66e2c81ca201c6ff9f359c2e33f6189563f9ed
mrdel1r1ous <alexsmirnov7428@gmail.com>
2025-11-15 01:37:11 +0200

    Updated main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -13,6 +13,7 @@ int main(int argc, char *argv[]) {
 	auto line = lab::createLine("7th_lab");
 	int x = lab::countChars(line, isDigit);
 	std::cout << line->content << std::endl;
+	std::cout << x << std::endl;
 	lab::deleteLine(line);
 	return EXIT_SUCCESS;
 }
