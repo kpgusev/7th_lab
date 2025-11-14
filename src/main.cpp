e736af1dd85122743a1df2f165d863ff2fbfde69
mrdel1r1ous <alexsmirnov7428@gmail.com>
2025-11-15 01:49:17 +0200

    Updated main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -4,14 +4,10 @@
 #include "data.h"
 #include "operations.h"
 
-bool isDigit(char c) {
-	if (isdigit(c)) return true;
-	return false;
-}
-
 int main(int argc, char *argv[]) {
 	auto line = lab::createLine("7th_lab");
-	int x = lab::countChars(line, isDigit);
+	int x = lab::countChars(line, lab::isDigit);
+	lab::replaceChars(line, lab::isVowel, '*');
 	std::cout << line->content << std::endl;
 	std::cout << x << std::endl;
 	lab::deleteLine(line);
